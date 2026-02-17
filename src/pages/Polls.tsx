0eb627840f9f69f11741bdf410bf841a8331cc31
tiffanywow <tiffany@wow.co.na>
2026-02-17 14:20:13 +0200

    Updated Polls.tsx

--- src/pages/Polls.tsx
+++ src/pages/Polls.tsx
@@ -565,16 +565,16 @@ export default function Polls() {
                           },
                         }}
                         modifiersClassNames={{
-                          inRange: 'bg-[#fecaca] !border !border-black',
-                          rangeEdge: '!border !border-black',
+                          inRange: 'bg-[#fecaca] border-transparent',
+                          rangeEdge: 'border-transparent',
                         }}
                         classNames={{
                           day: cn(
                             buttonVariants({ variant: 'ghost' }),
                             'h-8 w-8 p-0 font-normal bg-white hover:bg-white border border-gray-400 hover:border-gray-600'
                           ),
-                          day_today: 'bg-[#d1242a] text-white font-semibold hover:bg-[#d1242a] hover:text-white !border !border-black',
-                          day_selected: 'bg-[#d1242a] text-white font-semibold hover:bg-[#d1242a] hover:text-white focus:bg-[#d1242a] focus:text-white !border !border-black',
+                          day_today: 'bg-[#d1242a] text-white font-semibold hover:bg-[#d1242a] hover:text-white border-transparent',
+                          day_selected: 'bg-[#d1242a] text-white font-semibold hover:bg-[#d1242a] hover:text-white focus:bg-[#d1242a] focus:text-white border-transparent',
                           day_disabled: 'bg-white text-gray-400 opacity-50 cursor-not-allowed hover:bg-white border border-gray-300',
                           day_outside: 'text-gray-300 opacity-30 border-transparent bg-white',
                         }}
