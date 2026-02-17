3950f9a39b5e79b6f294fd6d33c9ac07f16e32b8
tiffanywow <tiffany@wow.co.na>
2026-02-17 12:59:43 +0200

    Updated calendar.tsx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -48,7 +48,7 @@ function Calendar({
         day_range_end: 'day-range-end',
         day_selected:
           'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
-        day_today: 'bg-[#d1242a] text-white font-semibold',
+        day_today: '!bg-[#d1242a] !text-white font-bold hover:!bg-[#d1242a] hover:!text-white',
         day_outside:
           'day-outside text-muted-foreground opacity-50  aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
         day_disabled: 'text-muted-foreground opacity-50',
